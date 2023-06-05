8fba764954c282a3ed711d97e02338f38ed4dbe2
VAXA99 <104323871+VAXA99@users.noreply.github.com>
2023-06-06 02:38:02 +0300

    updated entities with lombok

--- src/main/java/com/dab_userforum/Entity/Message.java
+++ src/main/java/com/dab_userforum/Entity/Message.java
@@ -1,5 +1,12 @@
 package com.dab_userforum.Entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Message {
 
     private Integer id;
@@ -12,61 +19,10 @@ public class Message {
 
     private String timestamp;
 
-    public Message() {
-    }
-
     public Message(String messageContent, Integer userId, String login, String timestamp) {
         this.messageContent = messageContent;
         this.userId = userId;
         this.login = login;
-        this.timestamp = timestamp;
-    }
-
-    public Message(Integer id, String messageContent, Integer userId, String login, String timestamp) {
-        this.id = id;
-        this.messageContent = messageContent;
-        this.userId = userId;
-        this.login = login;
-        this.timestamp = timestamp;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getMessageContent() {
-        return messageContent;
-    }
-
-    public void setMessageContent(String messageContent) {
-        this.messageContent = messageContent;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
         this.timestamp = timestamp;
     }
 }
--- src/main/java/com/dab_userforum/Entity/User.java
+++ src/main/java/com/dab_userforum/Entity/User.java
@@ -1,5 +1,12 @@
 package com.dab_userforum.Entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
 
     private Integer id;
@@ -16,81 +23,12 @@ public class User {
 
     private String role;
 
-    public User() {
-    }
-
     public User(String login, String password, String dateOfRegistration, String emailAddress, Integer numberOfMessages, String role) {
         this.login = login;
         this.password = password;
         this.dateOfRegistration = dateOfRegistration;
         this.emailAddress = emailAddress;
         this.numberOfMessages = numberOfMessages;
-        this.role = role;
-    }
-
-    public User(Integer id, String login, String password, String dateOfRegistration, String emailAddress, Integer numberOfMessages, String role) {
-        this.id = id;
-        this.login = login;
-        this.password = password;
-        this.dateOfRegistration = dateOfRegistration;
-        this.emailAddress = emailAddress;
-        this.numberOfMessages = numberOfMessages;
-        this.role = role;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getDateOfRegistration() {
-        return dateOfRegistration;
-    }
-
-    public void setDateOfRegistration(String dateOfRegistration) {
-        this.dateOfRegistration = dateOfRegistration;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public Integer getNumberOfMessages() {
-        return numberOfMessages;
-    }
-
-    public void setNumberOfMessages(Integer numberOfMessages) {
-        this.numberOfMessages = numberOfMessages;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
         this.role = role;
     }
 }
